8a56021f0e0cee4da0feb0d2927157dcf2f1f224
Richard Wurth <richard.wurth91@gmail.com>
2017-02-05 00:20:01 -0700

    Function Calculator - Open and close animation and icon rotate

--- javascript.js
+++ javascript.js
@@ -1,37 +1,25 @@
-$(document).ready(function () {
-
-  $('.to-shake').click(function () {
-    $(".to-shake").html(function (e) {
-      if (Math.floor(Math.random()*20) <= 10) {
-        $(".to-shake").effect("bounce",2000,"slow");
-        return "Bounce!";
-      } else {
-        $(".to-shake").effect("shake", 2000, "slow");
-        return "Shake it up!";
-      }
-    });
+$(document).ready(function(){
+  //DO NOT TOUCH//
+  var open = false;
+  $('#topImg').click(function(){
+    if (open) {
+      console.log('close');
+      open = false;
+      setTimeout(function(){
+        $('#topImg').toggleClass('topNavRotate');
+        $('.topNav').css('width','50px');
+      }, 500);
+      $('#mainPartClosed').css('height','0px');
+    } else {
+      console.log('open');
+      open = true;
+      $('#topImg').toggleClass('topNavRotate');
+      $('.topNav').css('width','300px');
+      setTimeout(function(){
+        $('#mainPartClosed').css('height','400px');
+        // $('#mainPartClosed').css('width','400px');
+      }, 300);
+    }
   });
-
-  $('button').click(function() {
-    $('body').css("background-color", function() {
-      let diceRoll = Math.floor(Math.random()*3);
-      if (diceRoll === 0) {
-        $('.result').html("WHAT HAVE YOU DONE?!");
-        $('.result').effect("shake","slow");
-        $('.to-shake').effect("explode","slow");
-        $('button').effect("explode","slow");
-        $('.result').effect("explode","slow");
-        return "green";
-      } else if (diceRoll === 1) {
-        return "blue";
-      } else if (diceRoll === 2) {
-        return "yellow";
-      } else if (diceRoll === 3) {
-        return "orange";
-      } else {
-        return "black";
-      }
-
-    });
-  });
+  //ENDING//
 });
